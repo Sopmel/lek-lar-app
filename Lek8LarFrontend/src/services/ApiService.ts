7d6052d15c1feaee450e7f53683df6853728cbe1
Sopmel <soph.melin@gmail.com>
2025-05-09 19:24:54 +0200

    add api to response url

--- Lek8LarFrontend/src/services/ApiService.ts
+++ Lek8LarFrontend/src/services/ApiService.ts
@@ -2,20 +2,20 @@ import axios, { AxiosRequestConfig } from "axios";
 
 class ApiService {
     readonly BASE_URL = process.env.REACT_APP_API_URL;
+    readonly BASE_URL_LOCAL = process.env.REACT_APP_API_URL_LOCAL;
 
 
 
     async post(endpoint: string, data?: any, config?: AxiosRequestConfig) {
         console.log("API URL:", process.env.REACT_APP_API_URL);
-
-        const response = await axios.post(`${this.BASE_URL}/${endpoint}`, data, config);
+        const response = await axios.post(`${this.BASE_URL}/api/${endpoint}`, data, config);
         return response.data;
     }
 
     async get(endpoint: string, params?: Record<string, string>) {
         console.log("API URL from get:", process.env.REACT_APP_API_URL);
 
-        const response = await axios.get(`${this.BASE_URL}/${endpoint}`, { params });
+        const response = await axios.get(`${this.BASE_URL}/api/${endpoint}`, { params });
         return response.data;
     }
 }
