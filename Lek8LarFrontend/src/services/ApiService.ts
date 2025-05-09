e1cf61861c8a5be087ac3a3e4ad672d7285ca45b
Sopmel <soph.melin@gmail.com>
2025-05-09 21:50:45 +0200

    testing

--- Lek8LarFrontend/src/services/ApiService.ts
+++ Lek8LarFrontend/src/services/ApiService.ts
@@ -1,21 +1,20 @@
 import axios, { AxiosRequestConfig } from "axios";
 
 class ApiService {
-    readonly BASE_URL = process.env.REACT_APP_API_URL;
-    readonly BASE_URL_LOCAL = process.env.REACT_APP_API_URL_LOCAL;
-
-
+    readonly BASE_URL =
+        process.env.NODE_ENV === "development"
+            ? process.env.REACT_APP_API_URL_LOCAL
+            : process.env.REACT_APP_API_URL;
 
     async post(endpoint: string, data?: any, config?: AxiosRequestConfig) {
-        console.log("API URL:", process.env.REACT_APP_API_URL);
-        const response = await axios.post(`${this.BASE_URL}/api/${endpoint}`, data, config);
+        console.log("POST to:", `${this.BASE_URL}/${endpoint}`);
+        const response = await axios.post(`${this.BASE_URL}/${endpoint}`, data, config);
         return response.data;
     }
 
     async get(endpoint: string, params?: Record<string, string>) {
-        console.log("API URL from get:", process.env.REACT_APP_API_URL);
-
-        const response = await axios.get(`${this.BASE_URL}/api/${endpoint}`, { params });
+        console.log("GET from:", `${this.BASE_URL}/${endpoint}`);
+        const response = await axios.get(`${this.BASE_URL}/${endpoint}`, { params });
         return response.data;
     }
 }
