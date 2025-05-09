47ea7d988ab96c9830bb7a9f6c293d81c18182ec
Sopmel <soph.melin@gmail.com>
2025-05-09 17:33:17 +0200

    commit

--- Lek8LarFrontend/src/services/ApiService.ts
+++ Lek8LarFrontend/src/services/ApiService.ts
@@ -1,15 +1,15 @@
 import axios, { AxiosRequestConfig } from "axios";
 
 class ApiService {
-    private baseUrl = "http://localhost:5000/api";
+    readonly BASE_URL = process.env.REACT_APP_API_URL;
 
     async post(endpoint: string, data?: any, config?: AxiosRequestConfig) {
-        const response = await axios.post(`${this.baseUrl}/${endpoint}`, data, config);
+        const response = await axios.post(`${this.BASE_URL}/${endpoint}`, data, config);
         return response.data;
     }
 
     async get(endpoint: string, params?: Record<string, string>) {
-        const response = await axios.get(`${this.baseUrl}/${endpoint}`, { params });
+        const response = await axios.get(`${this.BASE_URL}/${endpoint}`, { params });
         return response.data;
     }
 }
